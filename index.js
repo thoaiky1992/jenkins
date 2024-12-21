f3781d6d7546a3c8dafc4707ddf45f240323db90
thoaiky1992 <thoaiky1992@gmail.com>
2024-12-21 17:32:41 +0700

    update(6)

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (_, res) => {
-  res.json({ message: "Hello KySomaio đẹp trai và kute phô mai que , hehe !!!" });
+  res.json({ message: "Hello KySomaio đẹp trai và kute phô mai que , hehe !!!", statusCode: 200 });
 });
 
 app.listen(PORT, () => console.log(`server starting on port ${PORT}`));

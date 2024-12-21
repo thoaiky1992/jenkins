2d5b377996ec2ad1fa51907e55a821943dd56e3f
thoaiky1992 <thoaiky1992@gmail.com>
2024-12-21 15:42:48 +0700

    update ecs task and service

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (_, res) => {
-  res.json({ message: "Hello world !!!" });
+  res.json({ message: "Hello KySomaio !!!" });
 });
 
 app.listen(PORT, () => console.log(`server starting on port ${PORT}`));
